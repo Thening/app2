58b84d955b0937ed79c75397baffb8c73b5e447e
thening Diouf <thening@Rolands-MacBook-Pro.local>
2020-07-03 18:32:40 +0000

    Hello World Command Line

---
+++ hello.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var username = process.argv[2];
+
+if(!username) {
+    var appName = process.argv[1].split(require('path').sep).pop();
+
+    console.error('Missing argumemt! Example: %s YOUR_NAME', appName);
+    process.exit(1);
+}
+
+console.log('Hello %s!', username);
